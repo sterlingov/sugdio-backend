21d0951e854eb93b9f277ddc9f821d4a2293b91f
sterlingov <forwardeleven13@gmail.com>
2026-04-28 18:02:11 +0300

    refactor: handlers per entity split

---
+++ internal/handlers/shift.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"context"
+	"sugdio/api"
+)
+
+func (h *Handler) CreateShift(ctx context.Context, request api.CreateShiftRequestObject) (api.CreateShiftResponseObject, error) {
+	panic("unimplemented")
+}
+
+func (h *Handler) DeleteShift(ctx context.Context, request api.DeleteShiftRequestObject) (api.DeleteShiftResponseObject, error) {
+	panic("unimplemented")
+}
+
+func (h *Handler) GetShift(ctx context.Context, request api.GetShiftRequestObject) (api.GetShiftResponseObject, error) {
+	panic("unimplemented")
+}
+
+func (h *Handler) GetShifts(ctx context.Context, request api.GetShiftsRequestObject) (api.GetShiftsResponseObject, error) {
+	panic("unimplemented")
+}
+
+func (h *Handler) PatchShift(ctx context.Context, request api.PatchShiftRequestObject) (api.PatchShiftResponseObject, error) {
+	panic("unimplemented")
+}
